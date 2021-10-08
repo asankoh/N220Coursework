bf703718192f3fca5693c3c3a8f3003d2598a11b
asankoh <89537452+asankoh@users.noreply.github.com>
2021-10-08 10:04:48 -0400

    Hm

---
+++ objectsAssignment/js/app3.js
@@ -0,0 +1,32 @@
+let yPos=1
+
+let shape = [
+{  width: 100, height: 100, size: 50, color: "#4c405e"},
+{  width: 75, height: 25, color: "#FFFF00" },
+{  width: 50, height: 50, color: "#alc4d0"}
+
+];
+
+
+function setup() {
+    createCanvas(300,300);
+}
+
+function draw(){
+    background(130,10,200);
+    fill(shape[0].color)
+    circle(150,circleSize(),shape[0].width, shape[0].height,)
+    rect(25,25,shape[1].width,shape[1].height)
+}
+function circleSize(){
+    yPos = yPos + 5
+    if(yPos > 200){
+        shape[1].width--
+        i-- 
+        yPos = 0
+    }
+    for(i=0;i<100;i = i=i+.05){
+        circle(yPos + [i], 150, shape[0].width, shape[0].height)
+    }
+    }
+
