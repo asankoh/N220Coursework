696d70de833799f4ff8e72e577d6b73c5986f9c2
asankoh <89537452+asankoh@users.noreply.github.com>
2021-10-08 10:37:15 -0400

    oh

--- objectsAssignment/js/app3.js
+++ objectsAssignment/js/app3.js
@@ -3,7 +3,7 @@ let yPos=1
 let shape = [
 {  width: 100, height: 100, size: 50, color: "#4c405e"},
 {  width: 75, height: 25, color: "#FFFF00" },
-{  width: 50, height: 50, color: "#alc4d0"}
+{  width: 200, height: 25, color: "#alc4d0"}
 
 ];
 
@@ -13,16 +13,16 @@ function setup() {
 }
 
 function draw(){
-    background(130,10,200);
-    fill(shape[0].color)
-    circle(150,circleSize(),shape[0].width, shape[0].height,)
-    rect(25,25,shape[1].width,shape[1].height)
+    background(130,100,200);
+    fill(shape[2].color)
+    circle(mouseX,circleSize(),shape[0].width, shape[0].height,)
+    rect(0,0,shape[1].width,shape[1].height)
+
 }
 function circleSize(){
     yPos = yPos + 5
     if(yPos > 200){
-        shape[1].width--
-        i-- 
+        shape[1].width++
         yPos = 0
     }
     for(i=0;i<100;i = i=i+.05){
