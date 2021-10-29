e63d5d1650220845c8d7b92cf70c8be8cbb11924
asankoh <89537452+asankoh@users.noreply.github.com>
2021-10-29 10:51:41 -0400

    ohhh

--- userInput/js/app2.js
+++ userInput/js/app2.js
@@ -1,6 +1,7 @@
 let tipInput = document.getElementById("tipInput")
-
 function calculate(){
     let tip = 1 + Math.floor(Math.random() * 5)
-    console.log("Tip: " + tip)
+    let total = Number(tipInput.value)
+
+    console.log("Tip:$" + tip, "Total:$" + (total + tip))
 }
