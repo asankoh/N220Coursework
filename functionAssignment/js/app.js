1c4509a70034640f9ed9ea29970957c7e684b088
asankoh <89537452+asankoh@users.noreply.github.com>
2021-11-04 22:53:45 -0400

    boo

---
+++ functionAssignment/js/app.js
@@ -0,0 +1,12 @@
+let txtInput = document.getElementById("txtInput")
+
+
+function Divisible(){
+if (txtInput.value % 7){
+console.log("False") 
+} else{
+    console.log("True")
+}
+
+}
+Divisible();
---
+++ functionAssignment/js/app2.js
@@ -0,0 +1,10 @@
+
+function randomNumber(){
+ let random = 1 + Math.floor(Math.random() * 10)
+
+}
+
+function generate(random){
+    randomNumber();
+}
+generate();
