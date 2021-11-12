17ec28b460f5fc8d468274cfed9acc2ae0917fc8
asankoh <89537452+asankoh@users.noreply.github.com>
2021-11-12 02:00:18 -0500

    hmmm

---
+++ arrayComp/js/app.js
@@ -0,0 +1,12 @@
+let txtInput = document.getElementById("txtInput")
+function average(){
+
+
+let stringNums = txtInput.value;
+let numberArray = stringNums.split (",")
+
+
+} 
+
+
+console.log(stringNums)
---
+++ arrayCompExample/js/app.js
@@ -0,0 +1,26 @@
+ let myArray  = [5,2,5,7]
+
+/*let sum2 = myArray.reduce(doSum)
+
+console.log(sum2)
+
+function doSum(sum,val) {
+    return sum + val;
+}
+*/
+
+
+
+/*let sum2 = myArray.reduce( (sum,val) => {
+    return sum + val;
+});
+console.log(sum2);
+*/
+
+
+/* let sum2 = myArray.reduce((sum,val) => sum +val)
+console.log(sum2)
+*/
+// /* Commments all of your code!
+
+myArray.map(function (value) { console.log(value) }
