28ebf1000f96d1312126674a891121a101b5c543
asankoh <89537452+asankoh@users.noreply.github.com>
2021-12-10 23:30:30 -0500

    sigh

--- eventsAssignment/js/app3.js
+++ eventsAssignment/js/app3.js
@@ -1,14 +1,13 @@
-let colorChange = document.getElementById("eventChange");
-colorChange.addEventListener("click", itsChange);
-for(var i = 0; i < 3; i++){
-    colorChange.style.width = "200px" 
-    colorChange.style.height = "200px"
-    colorChange.style.backgroundColor = "#0000FF"
-    colorChange.style.margin = "5px"
-    }
-
-function itsChange(event){
-colorChange.style.backgroundColor = "#FFFFFF"
-colorChange.innerHTML = "Indianapolis"
+let cardDiv = document.querySelectorAll("cards")
+for (let i = 0; i < cardDiv.length; i++){
+cardDiv.addEventListener("click", itsChange);
 }
 
+
+function press(){
+    
+}
+
+function itsChange(event){
+    event.target.innerHTML = event.target.dataset.question;
+}
